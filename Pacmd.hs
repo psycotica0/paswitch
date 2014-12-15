0b86ebf87e151e35939bc8a40f39295082e8fcce
Christopher Vollick <0@psycoti.ca>
2014-12-15 16:31:37 -0500

    Return a Real Sink

--- Pacmd.hs
+++ Pacmd.hs
@@ -1,11 +1,13 @@
 module Pacmd where
 
 import System.Process (readProcess)
-import Text.Parsec (many, notFollowedBy, sepBy, space, manyTill, anyChar, char, parse, many1, (<|>), spaces, digit, string, Parsec, try)
+import Text.Parsec (many, notFollowedBy, sepBy, space, manyTill, anyChar, char, parse, many1, spaces, digit, string, Parsec, try)
+
+import Control.Applicative ((<|>), (<*>), (*>), (<*))
 
 endOfLine = char '\n'
 
-data Sink = Sink {sinkindex :: Int, sinkname :: String}
+data Sink = Sink {sinkindex :: Int, sinkname :: String} deriving (Show)
 
 data Input = Input {inputindex :: Int, inputname :: String, sink :: Int}
 
@@ -24,5 +26,11 @@ parse_sink = do
 	spaces
 	num <- many1 digit
 	endOfLine
-	many $ try $ spaces >> notFollowedBy (string "index:") >> manyTill anyChar endOfLine
-	return num
+	many $ try $ other_line
+	name <- name_line
+	many $ try $ other_line
+	return $ Sink (read num) name
+
+	where
+	name_line = spaces *> string "name:" *> spaces *> char '<' *> manyTill anyChar (char '>') <* manyTill anyChar endOfLine
+	other_line = spaces *> notFollowedBy (string "index:" <|> string "name:") *> manyTill anyChar endOfLine
