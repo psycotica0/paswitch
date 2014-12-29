7f13f7c1197c2e85cc44074cb9149f5c96012972
Christopher Vollick <0@psycoti.ca>
2014-12-29 12:22:07 -0500

    Added Error Handling
    I import ErrorT so I can handle parse errors.

--- switch.hs
+++ switch.hs
@@ -1,10 +1,20 @@
 import Pacmd
+import Control.Monad.Trans.Error (ErrorT(..), runErrorT, Error, strMsg)
+import Text.Parsec.Pos (initialPos)
+import Text.Parsec.Error (ParseError, newErrorMessage, Message(..))
+import Control.Monad.Trans.Class (lift)
+
+instance Error ParseError where
+	strMsg str = newErrorMessage (Message str) $ initialPos "Error"
 
 -- This finds the current default, and then takes the next one
 nextSink = head . tail . dropWhile (not . sinkdefault) . cycle
 
-main = do
-	inputs <- list_inputs
-	sinks <- list_sinks
-	print $ fmap nextSink sinks
-	putStrLn "Done"
+func = do
+	next_sink <- fmap nextSink $ ErrorT list_sinks
+	lift $ putStrLn $ "Switching default sink to " ++ sinkname next_sink
+
+main = handle =<< runErrorT func
+	where
+	handle (Left err) = putStrLn $ "Error:\n" ++ show err
+	handle (Right _) = return ()
